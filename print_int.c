05fa7b2e066bc27ced693da9c01327680e6c6b07
medayoubadri <medayoubadri@gmail.com>
2024-07-28 23:46:55 -0700

    Task 1: Commit for a _printf function that handles d and i specifiers

--- print_int.c
+++ print_int.c
@@ -6,29 +6,30 @@
  *
  * Return: The number of characters printed
  */
-int print_int(va_list args) {
-    int n = va_arg(args, int);
-    int num_chars = 0;
-    char buffer[20];
-    int i = 0;
+int print_int(va_list args)
+{
+	int n = va_arg(args, int);
+	int num_chars = 0;
+	char buffer[20];
+	int i = 0;
 
-    if (n == 0) {
-        return _putchar('0');
-    }
-
-    if (n < 0) {
-        num_chars += _putchar('-');
-        n = -n;
-    }
-
-    while (n > 0) {
-        buffer[i++] = (n % 10) + '0';
-        n /= 10;
-    }
-
-    while (i > 0) {
-        num_chars += _putchar(buffer[--i]);
-    }
-
-    return num_chars;
+	if (n == 0)
+	{
+		return (_putchar('0'));
+	}
+	if (n < 0)
+	{
+		num_chars += _putchar('-');
+		n = -n;
+	}
+	while (n > 0)
+	{
+		buffer[i++] = (n % 10) + '0';
+		n /= 10;
+	}
+	while (i > 0)
+	{
+		num_chars += _putchar(buffer[--i]);
+	}
+	return (num_chars);
 }
