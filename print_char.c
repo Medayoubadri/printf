69235eb9f9f59ea5ce37c3292893c42fcfb2a5f1
medayoubadri <medayoubadri@gmail.com>
2024-07-29 13:32:33 +0100

    Task 0: Update _printf function to handle edge cases and ensure proper counting of characters

--- print_char.c
+++ print_char.c
@@ -10,7 +10,6 @@ int print_char(va_list args)
 {
 	char c = va_arg(args, int);
 	
-	_putchar(c);
-	return (1);
+	return (_putchar(c));
 }
 
--- print_string.c
+++ print_string.c
@@ -11,7 +11,7 @@ int print_string(va_list args)
 	char *str = va_arg(args, char *);
 	int count = 0;
 
-	if (str == NULL)
+	if (!str)
 		str = "(null)";
 
 	while (*str)
