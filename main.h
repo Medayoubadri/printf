9fd221da54489093b19b4a9b837e429d74c0ef8c
medayoubadri <medayoubadri@gmail.com>
2024-07-29 20:48:14 -0700

    code updated

--- main.h
+++ main.h
@@ -1,12 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
 #include <unistd.h>
-
 
 /**
  * struct format - match the conversion specifiers for printf
@@ -15,34 +11,19 @@
  *
  */
 
-typedef struct format
+typedef struct convert_match
 {
-	char *id;
-	int (*f)();
+        char *id;
+        int (*f)(va_list);
 } convert_match;
 
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
-int _putchar(char c);
-int _printf(const char *format, ...);
 
-#endif /* MAIN.H */
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int print_int(va_list args);
+int print_dec(va_list args);
+int _putchar(char c);
+
+#endif /* MAIN_H */
