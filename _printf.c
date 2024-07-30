9213026cc59803056c8724d4dde906373045a450
medayoubadri <medayoubadri@gmail.com>
2024-07-29 21:44:18 -0700

    code updated for _printf

--- _printf.c
+++ _printf.c
@@ -8,37 +8,40 @@
  */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int i = 0, j, len = 0;
+	va_list args;
+	int i = 0, j, len = 0;
 
-        convert_match m[] = {
-                {"%c", print_char}, {"%s", print_string}, {"%%", print_percent},
-                {"%d", print_int}, {"%i", print_int},
-                {NULL, NULL}
-        };
+	convert_match m[] = {
+		{"%c", print_char}, {"%s", print_string}, {"%%", print_percent},
+		{"%d", print_int}, {"%i", print_int},
+		{NULL, NULL}
+	};
 
-        va_start(args, format);
-        if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-                return (-1);
+	va_start(args, format);
 
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	{
+		return (-1);
+	}
 Here:
-        while (format[i] != '\0')
-        {
-                j = 4;
-                while (j >= 0)
-                {
-                        if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-                        {
-                                len += m[j].f(args);
-                                i = i + 2;
-                                goto Here;
-                        }
-                        j--;
-                }
-                _putchar(format[i]);
-                len++;
-                i++;
-        }
-        va_end(args);
-        return (len);
+	while (format[i] != '\0')
+	{
+		j = 4;
+
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
 }
